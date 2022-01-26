9d3ab48319e18c9e761acee16695674145f5ab52
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2022-01-26 22:46:39 +0100

    Avoid creating new jwkset, shut down executor

--- jwt-server/jwk/jwk-connect2id/src/main/java/org/entur/jwt/jwk/connect2id/EnturJWKSource.java
+++ jwt-server/jwk/jwk-connect2id/src/main/java/org/entur/jwt/jwk/connect2id/EnturJWKSource.java
@@ -1,18 +1,18 @@
 package org.entur.jwt.jwk.connect2id;
 
-import java.util.List;
-
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.RemoteKeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKMatcher;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.entur.jwt.jwk.JwksException;
 import org.entur.jwt.jwk.JwksProvider;
 import org.entur.jwt.jwk.JwksUnavailableException;
 
-import com.nimbusds.jose.KeySourceException;
-import com.nimbusds.jose.RemoteKeySourceException;
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSelector;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
+import java.util.LinkedList;
+import java.util.List;
 
 public class EnturJWKSource<C extends SecurityContext> implements JWKSource<C> {
 
@@ -25,16 +25,33 @@ public class EnturJWKSource<C extends SecurityContext> implements JWKSource<C> {
 
     @Override
     public List<JWK> get(JWKSelector jwkSelector, C context) throws KeySourceException {
+
+        JWKMatcher matcher = jwkSelector.getMatcher();
+
         try {
-            List<JWK> select = jwkSelector.select(new JWKSet(provider.getJwks(false)));
+            List<JWK> select = select(provider.getJwks(false), matcher);
             if (select.isEmpty()) {
-                select = jwkSelector.select(new JWKSet(provider.getJwks(true)));
+                select = select(provider.getJwks(true), matcher);
             }
             return select;
         } catch (JwksUnavailableException e) {
             throw new RemoteKeySourceException("Unable to get keys", e);
         } catch (JwksException e) {
             throw new KeySourceException("Unable to get keys", e);
+        }
+    }
+
+    public List<JWK> select(List<JWK> list, JWKMatcher matcher) {
+        List<JWK> selectedKeys = new LinkedList();
+        if (list == null) {
+            return selectedKeys;
+        } else {
+            for (JWK jwk : list) {
+                if (matcher.matches(jwk)) {
+                    selectedKeys.add(jwk);
+                }
+            }
+            return selectedKeys;
         }
     }
 
