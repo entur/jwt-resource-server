889599f2a0963b05f6dfd9576e28ded77a6165a3
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2025-01-20 14:46:43 +0100

    Grpc ecosystem support (#89)
    * Add ecosystem support
    
    * Various adjustments, fix tests
    
    * Adjust config
    
    * Extract common code
    
    * Extract common code
    
    * Remove comment
    
    * Adjust config
    
    * Fix authorization check
    
    * Extract class
    
    * Adjsut interceptor
    
    * Bump new major version
    
    * Bump versions
    
    * Update docs
    
    * QA adjustments
    
    * Minor adjustments for some sonar-issues

---
+++ jwt-server/spring/jwt-spring-grpc-lognet/src/test/java/org/entur/jwt/spring/grpc/lognet/HealthCheckTest.java
@@ -0,0 +1,26 @@
+package org.entur.jwt.spring.grpc.lognet;
+
+import io.grpc.health.v1.HealthCheckRequest;
+import io.grpc.health.v1.HealthCheckResponse;
+import io.grpc.health.v1.HealthGrpc;
+import org.entur.jwt.junit5.AuthorizationServer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@AuthorizationServer
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+public class HealthCheckTest extends AbstractGrpcTest {
+
+	@Test
+	public void testGrpcHealth() throws Exception {
+		final HealthCheckRequest healthCheckRequest = HealthCheckRequest.newBuilder().build();
+		final HealthGrpc.HealthFutureStub healthFutureStub = HealthGrpc.newFutureStub(managedChannel);
+		final HealthCheckResponse.ServingStatus servingStatus = healthFutureStub.check(healthCheckRequest).get().getStatus();
+
+		assertEquals(servingStatus, HealthCheckResponse.ServingStatus.SERVING);
+	}
+}
