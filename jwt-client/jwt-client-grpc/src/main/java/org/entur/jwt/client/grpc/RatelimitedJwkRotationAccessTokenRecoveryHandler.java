e5fdb62d120a3a532d9292b519bb06b9a659be25
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2025-02-14 09:11:54 +0100

    Adjust queueing and tests

--- jwt-client/jwt-client-grpc/src/main/java/org/entur/jwt/client/grpc/RatelimitedJwkRotationAccessTokenRecoveryHandler.java
+++ jwt-client/jwt-client-grpc/src/main/java/org/entur/jwt/client/grpc/RatelimitedJwkRotationAccessTokenRecoveryHandler.java
@@ -6,9 +6,7 @@ import org.entur.jwt.client.AccessTokenProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.*;
 
 /**
  *
@@ -18,7 +16,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * There is however other reasons (i.e. bugs) to return UNAUTHENTICATED, so throttle how often the client
  * refreshes the access-token.
  * <br><br>
- * Refresh is performed in the background; there is no "rescue" for the failed call, i.e. key rotations with key
+ * Refresh is performed in the background; there is no "retry" for the failed call, i.e. key rotations with key
  * revocation is such a seldom event that some noise is acceptable.
  */
 
@@ -26,9 +24,7 @@ public class RatelimitedJwkRotationAccessTokenRecoveryHandler implements JwkRota
 
     private static final Logger log = LoggerFactory.getLogger(RatelimitedJwkRotationAccessTokenRecoveryHandler.class);
 
-    private final ExecutorService executorService;
-
-    private ReentrantLock reentrantLock = new ReentrantLock();
+    private final ThreadPoolExecutor executor;
 
     private final long minTimeInterval;
     private long nextOpeningTime = -1L;
@@ -37,51 +33,40 @@ public class RatelimitedJwkRotationAccessTokenRecoveryHandler implements JwkRota
     public RatelimitedJwkRotationAccessTokenRecoveryHandler(final long minTimeInterval) {
         this.minTimeInterval = minTimeInterval;
 
-        this.executorService = Executors.newSingleThreadExecutor();
+        this.executor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(), Executors.defaultThreadFactory());
     }
 
     @Override
     public void handle(AccessTokenProvider accessTokenProvider, String authorizationHeader, long currentTime) {
         try {
-            if(reentrantLock.tryLock()) {
-                try {
-                    queueRefresh(accessTokenProvider, authorizationHeader, currentTime);
-                } finally {
-                    reentrantLock.unlock();
-                }
-            } else {
-                // another thread is doing this
+            BlockingQueue<Runnable> queue = executor.getQueue();
+            // inaccurate, but good enough for this scenario
+            if(executor.getActiveCount() == 0 && queue.size() == 0) {
+                queueRefresh(accessTokenProvider, authorizationHeader, currentTime);
             }
         } catch(Throwable e) {
             log.warn("Problem queuing refreshing access-token in background", e);
         }
     }
 
-    private void queueRefresh(AccessTokenProvider accessTokenProvider, String header, long currentTime) throws AccessTokenException {
-        executorService.submit(() -> {
+    private void queueRefresh(AccessTokenProvider accessTokenProvider, String header, long currentTime) {
+        executor.submit(() -> {
             try {
                 AccessToken currentAccessToken = accessTokenProvider.getAccessToken(false);
-                if(header.endsWith(currentAccessToken.getValue())) {
-                    if (refresh(accessTokenProvider, currentTime)) {
-                        log.info("Force refreshed access-token");
-                    } else {
-                        log.info("Not refreshing access-token due to rate limiting");
-                    }
-                } else {
+                if (!header.endsWith(currentAccessToken.getValue())) {
                     // access-token already refreshed
+                    return;
                 }
-            } catch(Throwable e) {
-                log.warn("Problem force refresh access-token", e);
+                if (isRateLimited(currentTime)) {
+                    log.info("Not refreshing access-token due to rate limiting");
+                    return;
+                }
+                log.info("Force refreshed access-token");
+                accessTokenProvider.getAccessToken(true);
+            } catch (Throwable e) {
+                log.warn("Problem refreshing access-token", e);
             }
         });
-    }
-
-    private boolean refresh(AccessTokenProvider accessTokenProvider, long currentTime) throws AccessTokenException {
-        if (!isRateLimited(currentTime)) {
-            accessTokenProvider.getAccessToken(true);
-            return true;
-        }
-        return false;
     }
 
     protected boolean isRateLimited(long currentTime) {
@@ -102,10 +87,10 @@ public class RatelimitedJwkRotationAccessTokenRecoveryHandler implements JwkRota
     }
 
     public void close() {
-        executorService.close();
+        executor.close();
     }
 
-    public ExecutorService getExecutorService() {
-        return executorService;
+    protected ThreadPoolExecutor getExecutor() {
+        return executor;
     }
 }
--- jwt-client/jwt-client-grpc/src/test/java/org/entur/jwt/client/grpc/RatelimitedJwkRotationAccessTokenRecoveryHandlerTest.java
+++ jwt-client/jwt-client-grpc/src/test/java/org/entur/jwt/client/grpc/RatelimitedJwkRotationAccessTokenRecoveryHandlerTest.java
@@ -4,6 +4,9 @@ import org.entur.jwt.client.AccessToken;
 import org.entur.jwt.client.AccessTokenException;
 import org.entur.jwt.client.AccessTokenProvider;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -17,40 +20,52 @@ public class RatelimitedJwkRotationAccessTokenRecoveryHandlerTest {
     public void testForceRefresh() throws Exception {
         RatelimitedJwkRotationAccessTokenRecoveryHandler handler = new RatelimitedJwkRotationAccessTokenRecoveryHandler(30_000L);
 
-        AccessTokenProvider accessTokenProvider = mock(AccessTokenProvider.class);
-        String authorizationHeader = "Bearer x.y.z";
+        try {
+            AccessTokenProvider accessTokenProvider = mock(AccessTokenProvider.class);
+            String authorizationHeader = "Bearer x.y.z";
 
-        AccessToken accessToken = new AccessToken(authorizationHeader, "bearer", 0);
-        when(accessTokenProvider.getAccessToken(false)).thenReturn(accessToken);
+            AccessToken accessToken = new AccessToken(authorizationHeader, "bearer", 0);
+            when(accessTokenProvider.getAccessToken(false)).thenReturn(accessToken);
 
-        AccessToken refreshedAccessToken = new AccessToken("a.b.c", "bearer", 0);
-        when(accessTokenProvider.getAccessToken(true)).thenReturn(refreshedAccessToken);
+            AccessToken refreshedAccessToken = new AccessToken("a.b.c", "bearer", 0);
+            when(accessTokenProvider.getAccessToken(true)).thenReturn(refreshedAccessToken);
 
-        long timestamp = System.currentTimeMillis();
-        handler.handle(accessTokenProvider, authorizationHeader, timestamp);
+            long timestamp = System.currentTimeMillis();
+            handler.handle(accessTokenProvider, authorizationHeader, timestamp);
 
-        Thread.sleep(50);
+            ThreadPoolExecutor executor = handler.getExecutor();
+            long deadline = System.currentTimeMillis() + 1000;
+            while (System.currentTimeMillis() < deadline && (executor.getActiveCount() > 0 || executor.getQueue().size() > 0))
+                ;
 
-        verify(accessTokenProvider, times(1)).getAccessToken(false);
-        verify(accessTokenProvider, times(1)).getAccessToken(true);
+            verify(accessTokenProvider, times(1)).getAccessToken(false);
+            verify(accessTokenProvider, times(1)).getAccessToken(true);
+        } finally {
+            handler.close();
+        }
     }
 
     @Test
     public void testAlreadyRefreshed() throws Exception {
         RatelimitedJwkRotationAccessTokenRecoveryHandler handler = new RatelimitedJwkRotationAccessTokenRecoveryHandler(30_000L);
+        try {
+            AccessTokenProvider accessTokenProvider = mock(AccessTokenProvider.class);
+            String authorizationHeader = "Bearer x.y.z";
 
-        AccessTokenProvider accessTokenProvider = mock(AccessTokenProvider.class);
-        String authorizationHeader = "Bearer x.y.z";
+            AccessToken otherAccessToken = new AccessToken("a.b.c", "bearer", 0);
+            when(accessTokenProvider.getAccessToken(false)).thenReturn(otherAccessToken);
 
-        AccessToken otherAccessToken = new AccessToken("a.b.c", "bearer", 0);
-        when(accessTokenProvider.getAccessToken(false)).thenReturn(otherAccessToken);
+            long timestamp = System.currentTimeMillis();
+            handler.handle(accessTokenProvider, authorizationHeader, timestamp);
 
-        long timestamp = System.currentTimeMillis();
-        handler.handle(accessTokenProvider, authorizationHeader, timestamp);
+            ThreadPoolExecutor executor = handler.getExecutor();
+            long deadline = System.currentTimeMillis() + 1000;
+            while(System.currentTimeMillis() < deadline && (executor.getActiveCount() > 0 || executor.getQueue().size() > 0) );
 
-        Thread.sleep(50);
-
-        verify(accessTokenProvider, times(1)).getAccessToken(false);
-        verify(accessTokenProvider, times(0)).getAccessToken(true);
+            verify(accessTokenProvider, times(1)).getAccessToken(false);
+            verify(accessTokenProvider, times(0)).getAccessToken(true);
+        } finally {
+            handler.close();
+        }
     }
 }
